10ef26564a62b037255a60f28df3b2f47a7b48c0
salva <salvador0796@gmail.com>
2021-11-02 18:57:44 -0300

    Funcion de compraracion de stats terminada

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var contenedor_resultado2 = document.getElementById('contenedor-resultado-user2'
 
 const jotason = 'response.json'
 
-// pintarHeroeOVillano(contenedor_resultado)
-// pintarHeroeOVillano2(contenedor_resultado2)
-
 
 var estadisticasU1 = []
 var estadisticasU2 = []
@@ -20,61 +17,8 @@ boton.addEventListener('click', async function(){
     await pintarHeroeOVillano(contenedor_resultado)
     await pintarHeroeOVillano2(contenedor_resultado2)
 
-
     compararEstadisticas(estadisticasU1, estadisticasU2)
-
-
-    // .then(function(){
-    //     compararEstadisticas(estadisticasU1, estadisticasU2)
-    // })
-
-    
-
-    estadisticasU2 = []
-    estadisticasU1 = []
 })
-
-
-
-
-function compararEstadisticas(listaU1, listaU2){
-    if(listaU1[0] > listaU2[0]){
-        console.log("Usuario 1 gana")
-
-        contadorUsuario1++
-        document.getElementById('usuario1').innerHTML = contadorUsuario1
-
-        // console.log(estadisticasU1)
-        // console.log(estadisticasU2)
-
-    }else if(listaU1[0] < listaU2[0]){
-        console.log("Usuario 2 gana")
-
-        contadorUsuario2++
-        document.getElementById('usuario2').innerHTML = contadorUsuario2
-
-        // console.log(estadisticasU1)
-        // console.log(estadisticasU2)
-
-    }else if(listaU1[0] === listaU2[0]){
-        // console.log("Empate")
-        alert("Empate")
-
-        // console.log(estadisticasU1)
-        // console.log(estadisticasU2)
-    }
-
-    
-    console.log(estadisticasU1)
-    console.log(estadisticasU2)
-
-    console.log("Comparacion de estadisticas listas")
-}
-
-
-
-
-
 
 
 async function pintarHeroeOVillano(caja){
@@ -132,7 +76,7 @@ async function pintarHeroeOVillano(caja){
             data[rndInt].powerstats.combat,
             data[rndInt].powerstats.strength
         ]
-
+        console.log("----------------------------------")
         console.log("Heroe o Villano del jugador 1 listo")
 
     })
@@ -190,17 +134,108 @@ async function pintarHeroeOVillano2(caja){
 
         estadisticasU2 = [
             data[rndInt].powerstats.intelligence,
+            data[rndInt].powerstats.strength,
             data[rndInt].powerstats.speed,
             data[rndInt].powerstats.durability,
             data[rndInt].powerstats.power,
             data[rndInt].powerstats.combat,
-            data[rndInt].powerstats.strength
         ]
-
+        console.log("----------------------------------")
         console.log("Heroe o Villano del jugador 2 listo")
         
     })
     .catch(error => {
         console.log(error)
     })
+}
+
+
+
+function compararEstadisticas(listaU1, listaU2){
+    console.log("----------------------------------")
+    console.log("Comparacion de estadisticas listas")
+    console.log(estadisticasU1)
+    console.log(estadisticasU2)
+
+    var ganadorU1 = 0
+    var ganadorU2 = 0
+
+    // COMPARACION DE INTELIGENCIA
+    if(listaU1[0] > listaU2[0]){
+        ganadorU1++ // Si gana en inteligencia se le suma un punto
+        console.log("Gana U1 en inteligencia")
+    }else if(listaU1[0] < listaU2[0]){
+        ganadorU2++ // Si gana en inteligencia se le suma un punto
+        console.log("Gana U2 en inteligencia")
+    }else if(listaU1[0] === listaU2[0]){
+        console.log("Empate de inteligencia")
+    }
+
+    // COMPARACION DE FUERZA
+    if(listaU1[1] > listaU2[1]){
+        ganadorU1++
+        console.log("Gana U1 en fuerza")
+    }else if(listaU1[1] < listaU2[1]){
+        ganadorU2++
+        console.log("Gana U2 en fuerza")
+    }else if(listaU1[1] === listaU2[1]){
+        console.log("Empate de fuerza")
+    }
+
+    // COMPARACION DE VELOCIDAD
+    if(listaU1[2] > listaU2[2]){
+        ganadorU1++
+        console.log("Gana U1 en velocidad")
+    }else if(listaU1[2] < listaU2[2]){
+        ganadorU2++
+        console.log("Gana U2 en velocidad")
+    }else if(listaU1[2] === listaU2[2]){
+        console.log("Empate de velocidad")
+    }
+
+    // COMPARACION DE DURABILIDAD
+    if(listaU1[3] > listaU2[3]){
+        ganadorU1++
+        console.log("Gana U1 en durabilidad")
+    }else if(listaU1[3] < listaU2[3]){
+        ganadorU2++
+        console.log("Gana U2 en durabilidad")
+    }else if(listaU1[3] === listaU2[3]){
+        console.log("Empate de durabilidad")
+    }
+
+    // COMPARACION DE PODER
+    if(listaU1[4] > listaU2[4]){
+        ganadorU1++
+        console.log("Gana U1 en poder")
+    }else if(listaU1[4] < listaU2[4]){
+        ganadorU2++
+        console.log("Gana U2 en poder")
+    }else if(listaU1[4] === listaU2[4]){
+        console.log("Empate de poder")
+    }
+
+    // COMPARACION DE COMBATE
+    if(listaU1[5] > listaU2[5]){
+        ganadorU1++
+        console.log("Gana U1 en combate")
+    }else if(listaU1[5] < listaU2[5]){
+        ganadorU2++
+        console.log("Gana U2 en combate")
+    }else if(listaU1[5] === listaU2[5]){
+        console.log("Empate de combate")
+    }
+
+    if(ganadorU1 > ganadorU2){
+        console.log("Usuario 1 gana")
+        contadorUsuario1++
+        document.getElementById('usuario1').innerHTML = contadorUsuario1
+    }else if(ganadorU1 < ganadorU2){
+        console.log("Usuario 2 gana")
+        contadorUsuario2++
+        document.getElementById('usuario2').innerHTML = contadorUsuario2
+    }else if(ganadorU1 === ganadorU2){
+        console.log("Empate")
+        alert("Empate")
+    }
 }
